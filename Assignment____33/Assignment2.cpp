05ad831d258edf73cd52005f5b659afd5919714d
Bharat Sanjay Patil <rp9609717@gmail.com>
2025-11-18 21:46:44 +0530

    Add files via upload

---
+++ Assignment____33/Assignment2.cpp
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<stdbool.h>
+#include<iostream>
+using namespace std;
+
+typedef unsigned int UINT;
+void CommonBit(UINT iNo1 ,UINT iNo2 )
+{
+    UINT iResult = iNo1 & iNo2;
+
+    int iCount = 1;
+    while(iResult != 0)
+    {
+        if(iResult & 1)
+        {
+            printf("%d\t",iCount);
+        }
+        iResult = iResult >>1;
+        iCount++;
+    }
+    printf("\n");
+   
+}
+int main()
+{
+    UINT iValue1 = 0, iValue2 = 0;
+    
+    int iRet = 0;
+    printf("Enter First number : ");
+    scanf("%d",&iValue1);
+    printf("Enter Second number : ");
+    scanf("%d",&iValue2);
+
+    CommonBit(iValue1, iValue2);
+   
+    
+
+    return 0 ;
+}
